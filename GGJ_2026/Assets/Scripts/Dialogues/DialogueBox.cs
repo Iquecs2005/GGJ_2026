6b9d80240357ecfa945216a151e922bdbcb5897c
jomigmonte <jomigmonte@gmail.com>
2026-01-31 22:11:02 -0300

    dialogue start

---
+++ GGJ_2026/Assets/Scripts/Dialogues/DialogueBox.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DialogueBox : ScriptableObject
+{
+    public string text;
+    public Sprite char_sprite;
+    public string char_name;
+}
---
+++ GGJ_2026/Assets/Scripts/Dialogues/DialogueHandler.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject sprite;
+    [SerializeField] private TextMeshProUGUI dialogue;
+    [SerializeField] private TextMeshProUGUI charName;
+    [SerializeField] private TextMeshProUGUI[] nameLog;
+    [SerializeField] private GameObject dialogueBox;
+
+
+    [SerializeField] private DialogueBox[] dialogues;
+    private List<DialogueBox> dialoguesLog = new List<DialogueBox>();
+    private int index = 0;
+
+
+    void Start()
+    {
+        Sprite newSprite = dialogues[index].char_sprite;
+        sprite.GetComponent<Image>().sprite = newSprite;
+        dialogue.text = dialogues[index].text;
+        charName.text = dialogues[index].char_name;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            NextDialogue();
+        }
+    }
+
+    public void DialogueStart()
+    {
+        
+    }
+
+    public void NextDialogue()
+    {
+        if (index < dialogues.Length)
+        {
+            dialogue.text = dialogues[index].text;
+            charName.text = dialogues[index].char_name;
+            ++index;
+        }
+        else
+        {
+            index = 0;
+            DialogueEnd();
+        }
+    }
+
+    public void DialogueEnd()
+    {
+
+    }
+}
+
